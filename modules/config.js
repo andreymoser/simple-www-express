e3d993fae19dcd1bf902c9b79f3fd4c3ace680c7
abidinotto <andrey.bidinotto@e-core.com>
2017-02-13 10:40:55 -0200

    minor changes

--- modules/config.js
+++ modules/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   "mongod": {
-    "url": 'mongodb://ecore:ecore@ds119788.mlab.com:19788/ecore-nodejs'
+    "url": 'mongodb://guest:guest@ds151289.mlab.com:51289/simple-www-express'
   },
   "jsonwebtoken": {
     "secret": '42 is the answer.'
